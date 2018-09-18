58c3bd0def8dcc7e1560361872021d6d2b8b4f46
Nitish Kumar Dwivedi <nitish.dwi@gmail.com>
2018-09-18 19:53:23 +0530

    Revert "Multithread"

--- Client.cpp
+++ Client.cpp
@@ -12,17 +12,20 @@
 #include <netdb.h>
 #include <bits/stdc++.h>
 #include <unistd.h>
-#include <thread>
 using namespace std;
 
-#define PORT 2000
-#define LENGTH 1025
+#define PORT 20000
+#define LENGTH 1024
 
-vector<thread> TH;
-int ThreadC;
-void filedownload(int portnum,int cnt)
+
+void error(const char *msg)
 {
+    perror(msg);
+    exit(1);
+}
 
+int main(int argc, char *argv[])
+{
     int sockfd; 
     int nsockfd;
     char buffer[LENGTH]; 
@@ -35,7 +38,7 @@ void filedownload(int portnum,int cnt)
     }
 
     remote_addr.sin_family = AF_INET; 
-    remote_addr.sin_port = htons(portnum); 
+    remote_addr.sin_port = htons(PORT); 
     //remote_addr.sin_addr.s_addr = INADDR_ANY;
     inet_pton(AF_INET, "127.0.0.1", &remote_addr.sin_addr); 
     bzero(&(remote_addr.sin_zero), 8);
@@ -46,44 +49,45 @@ void filedownload(int portnum,int cnt)
         exit(1);
     }
     else 
-        printf("[Client] Connected to server at port %d...ok!\n", portnum);
+        printf("[Client] Connected to server at port %d...ok!\n", PORT);
 
-    string fr_name = "recieve" + to_string(cnt)+ ".png";
-    FILE *fr = fopen(fr_name.c_str(), "wb");
+    printf("[Client] Receiveing file from Server and saving it as final.txt...");
+    char* fr_name = "final.pdf";
+    FILE *fr = fopen(fr_name, "a");
     if(fr == NULL)
         printf("File %s Cannot be opened.\n", fr_name);
     else
     {
         bzero(buffer, LENGTH); 
         int fr_block_sz = 0;
-        while((fr_block_sz = read(sockfd, buffer, LENGTH-1)) > 0)
+        while((fr_block_sz = recv(sockfd, buffer, LENGTH, 0)) > 0)
+        {
             int write_sz = fwrite(buffer, sizeof(char), fr_block_sz, fr);
+            if(write_sz < fr_block_sz)
+            {
+                error("File write failed.\n");
+            }
+            bzero(buffer, LENGTH);
+            if (fr_block_sz == 0 || fr_block_sz != LENGTH) 
+            {
+                break;
+            }
+        }
+        if(fr_block_sz < 0)
+        {
+            if (errno == EAGAIN)
+            {
+                printf("recv() timed out.\n");
+            }
+            else
+            {
+                fprintf(stderr, "recv() failed due to errno = %d\n", errno);
+            }
+        }
         printf("Ok received from server!\n");
         fclose(fr);
     }
     close (sockfd);
     printf("[Client] Connection lost.\n");
-    // pthread_exit(EXIT_SUCCESS);
-
-}
-
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
-
-int main(int argc, char *argv[])
-{
-
-    ThreadC = 40;
-    for(int i =0; i< ThreadC; ++i)
-    {
-        TH.push_back(thread(filedownload, 2000,i));
-        
-    }
-    for(auto &th : TH)
-        if(th.joinable()) th.join();
-        
     return (0);
 }
